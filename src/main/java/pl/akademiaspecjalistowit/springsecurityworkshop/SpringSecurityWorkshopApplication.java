5f988ec4f507e8b076cda0a78123ddebd72a9aea
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2024-01-09 00:10:51 +0100

    init commit

---
+++ src/main/java/pl/akademiaspecjalistowit/springsecurityworkshop/SpringSecurityWorkshopApplication.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.springsecurityworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/akademiaspecjalistowit/springsecurityworkshop/SpringSecurityWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.springsecurityworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
